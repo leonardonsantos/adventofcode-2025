75148c1bc48a552cacae5381dbdd0b00dcebfc5b
Leonardo Nascimento dos Santos <leonardo.nascimento.dos.santos@volvocars.com>
2025-12-19 15:36:12 +0100

    day02 part1

---
+++ src/main/kotlin/Day02.kt
@@ -0,0 +1,39 @@
+package org.example
+
+import java.io.File
+
+object Day02 {
+    data class Range(val start: Long, val end: Long)
+
+    fun readInput(filename: String): List<Range> =
+        File(filename).readText().split(",").map { rangeStr ->
+            val bounds = rangeStr.split("-")
+            val range = Range(bounds[0].toLong(), bounds[1].toLong())
+            range
+        }
+
+    fun solution1(ranges: List<Range>): Long {
+        var sum: Long = 0
+        ranges.forEach { range ->
+            for (i in range.start..range.end) {
+                val valueStr = i.toString()
+                if (valueStr.length % 2 == 0) {
+                    val mid = valueStr.length / 2
+                    val firstHalf = valueStr.substring(0, mid)
+                    val secondHalf = valueStr.substring(mid)
+                    if (firstHalf == secondHalf) {
+                        sum += i
+                    }
+                }
+            }
+        }
+        return sum
+    }
+}
+
+fun main() {
+    val input1 = Day02.readInput("./src/main/resources/day02/input1.txt")
+    println(input1)
+    val result1 = Day02.solution1(input1)
+    println("Result 1: $result1")
+}
